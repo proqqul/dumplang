a553ffb885dcaa0b459d0ca1402c62f761048a9d
Owen Lynch <root@owenlynch.org>
2023-03-28 14:12:28 -0700

    initial structure

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ zig zls ];
+      };
+    };
+}
